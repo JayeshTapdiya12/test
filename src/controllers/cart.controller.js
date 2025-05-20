e857993e4aec87f3210f0b39b6db36699a13a13f
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-05-20 11:15:31 +0530

    mirror update in controllers

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,72 +1,67 @@
-import HttpStatus from 'http-status-codes';
-import *  as cartService from '../services/cart.service';
+import HttpsStatus from 'http-status-codes';
+import *as cartService from '../services/cart.service'
 
 
-
-export const getCart = async (req, res, next) => {
+export const getCart = async (req, res) => {
     try {
-        const data = await cartService.getCart(req.body);
-        res.status(HttpStatus.ACCEPTED).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: 'Successfullt acceptted'
-        })
+        const data = await cartService.getCart(req.user);
+        res.status(HttpsStatus.OK).json({
+            code: HttpsStatus.OK,
+            data,
+            message: "Successfully fetched cart"
+        });
     } catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            message: `Cannot fetch the cart: ${error}`
-        })
+        res.status(HttpsStatus.BAD_REQUEST).json({
+            code: HttpsStatus.BAD_REQUEST,
+            message: `Cannot fetch the cart: ${error.message}`
+        });
     }
-}
+};
 
-
-export const getProduct = async (req, res, next) => {
+export const getProduct = async (req, res) => {
     try {
-        const data = await cartService.getProduct(req.params._id, req.body);
-        res.status(HttpStatus.OK).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: 'Successfullt got the product'
-        })
+        const data = await cartService.getProduct(req.params._id, req.user);
+        res.status(HttpsStatus.OK).json({
+            code: HttpsStatus.OK,
+            data,
+            message: "Successfully fetched product in cart"
+        });
     } catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            message: `Not getting the product: ${error}`
-        })
+        res.status(HttpsStatus.BAD_REQUEST).json({
+            code: HttpsStatus.BAD_REQUEST,
+            message: `Not getting the product: ${error.message}`
+        });
     }
-}
+};
 
-
-export const addProduct = async (req, res, next) => {
+export const addProduct = async (req, res) => {
     try {
-        const data = await cartService.addProduct(req.params, req.body);
-        res.status(HttpStatus.ACCEPTED).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: "Product is Added in the cart"
-        })
-
+        const data = await cartService.addProduct(req.params._id, req.user);
+        res.status(HttpsStatus.OK).json({
+            code: HttpsStatus.OK,
+            data,
+            message: "Product added to cart"
+        });
     } catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            message: `Not adding the product the product: ${error}`
-        })
+        res.status(HttpsStatus.BAD_REQUEST).json({
+            code: HttpsStatus.BAD_REQUEST,
+            message: `Not adding the product: ${error.message}`
+        });
     }
-}
+};
 
-export const removeProduct = async (req, res, next) => {
+export const removeProduct = async (req, res) => {
     try {
-        const data = await cartService.removeProduct(req.params, req.body);
-        res.status(HttpStatus.ACCEPTED).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: "Product is Delete in the cart"
-        })
-
+        const data = await cartService.removeProduct(req.params._id, req.user);
+        res.status(HttpsStatus.OK).json({
+            code: HttpsStatus.OK,
+            data,
+            message: "Product removed from cart"
+        });
     } catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            message: `Not deleting the product the product: ${error}`
-        })
+        res.status(HttpsStatus.BAD_REQUEST).json({
+            code: HttpsStatus.BAD_REQUEST,
+            message: `Not deleting the product: ${error.message}`
+        });
     }
-}
+};
--- src/controllers/wishlisht.controller.js
+++ src/controllers/wishlisht.controller.js
@@ -1,51 +1,54 @@
 import HttpsStatus from 'http-status-codes';
+import * as wishlistService from '../services/wishlist.service';
 
-import *  as wishlistService from '../services/wishlist.service';
-
-export const getWishlist = async (req, res, next) => {
+export const getWishlist = async (req, res) => {
     try {
-        const data = await wishlistService.getWishlist(req.body.createdBy);
-        res.status(HttpsStatus.ACCEPTED).json({
-            code: HttpsStatus.ACCEPTED,
-            data: data,
-            message: 'succesfully fetched the wiishlist'
-        })
+        const createdBy = req.user._id;
+        const data = await wishlistService.getWishlist(createdBy);
+        res.status(HttpsStatus.OK).json({
+            code: HttpsStatus.OK,
+            data,
+            message: 'Successfully fetched the wishlist'
+        });
     } catch (error) {
         res.status(HttpsStatus.BAD_REQUEST).json({
             code: HttpsStatus.BAD_REQUEST,
-            message: `can't access the wishlist : ${error}`
-        })
+            message: `Can't access the wishlist: ${error.message}`
+        });
     }
-}
+};
 
 export const addWishProduct = async (req, res, next) => {
     try {
-        const data = await wishlistService.addWishProduct(req.params, req.body.createdBy);
+        const data = await wishlistService.addWishProduct(req.params._id, req.user.id);
         res.status(HttpsStatus.ACCEPTED).json({
             code: HttpsStatus.ACCEPTED,
             data: data,
-            message: 'succesfully fetched the wiishlist'
-        })
+            message: 'Successfully added to wishlist'
+        });
     } catch (error) {
         res.status(HttpsStatus.BAD_REQUEST).json({
             code: HttpsStatus.BAD_REQUEST,
-            message: `can't add the product the wishlist : ${error}`
-        })
+            message: `Can't add the product to wishlist: ${error.message}`
+        });
     }
-}
+};
 
-export const removeWishProduct = async (req, res, next) => {
+
+
+export const removeWishProduct = async (req, res) => {
     try {
-        const data = await wishlistService.removeWishProduct(req.params, req.body.createdBy);
-        res.status(HttpsStatus.ACCEPTED).json({
-            code: HttpsStatus.ACCEPTED,
-            data: data,
-            message: 'succesfully remove the product  the wiishlist'
-        })
+        const createdBy = req.user._id;
+        const data = await wishlistService.removeWishProduct(req.params, createdBy);
+        res.status(HttpsStatus.OK).json({
+            code: HttpsStatus.OK,
+            data,
+            message: 'Product removed from wishlist successfully'
+        });
     } catch (error) {
         res.status(HttpsStatus.BAD_REQUEST).json({
             code: HttpsStatus.BAD_REQUEST,
-            message: `can't remove the product the wishlist : ${error}`
-        })
+            message: `Can't remove the product from wishlist: ${error.message}`
+        });
     }
-}
+};
